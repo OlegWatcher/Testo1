563d8aa378985b0f2e51a1c0089dd50d706dbb0d
OlegWatcher <olegstratilov@gmail.com>
2022-06-29 17:42:16 +0300

    создал функцию которая возвращает кол-во секунд

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const hour = 60 * minute;
 const day = 24 * hour;
 const week = 7 * day;
 console.log(minute, hour, day, week)
+
+
+function getSeconds(period, count) {
+    const minute = 60;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+    const week = 7 * day;
+
+    const seconds = {
+        minute,
+        hour,
+        day,
+        week
+    } 
+    
+    return seconds[period] * count;
+
+} 
+const result = getSeconds('day', 3)
+console.log(result)
